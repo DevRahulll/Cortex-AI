af32802db8e598782a86c4c6e7b7dcd5ea8ffc7f
Rahul <rahuldevan2019@gmail.com>
2026-09-24 23:46:41 +0530

    clerk initalize

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/provider/theme-provider";
 import { QueryProvider } from "@/components/provider/query-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -38,23 +38,25 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             suppressHydrationWarning
         >
             <body className="min-h-full flex flex-col">
-                <QueryProvider>
-                    <ThemeProvider
-                        attribute="class"
-                        defaultTheme="system"
-                        enableSystem
-                        disableTransitionOnChange
-                    >
+                <ClerkProvider>
+                    <QueryProvider>
                         <ThemeProvider
                             attribute="class"
                             defaultTheme="system"
                             enableSystem
                             disableTransitionOnChange
                         >
-                            {children}
+                            <ThemeProvider
+                                attribute="class"
+                                defaultTheme="system"
+                                enableSystem
+                                disableTransitionOnChange
+                            >
+                                {children}
+                            </ThemeProvider>
                         </ThemeProvider>
-                    </ThemeProvider>
-                </QueryProvider>
+                    </QueryProvider>
+                </ClerkProvider>
             </body>
         </html>
     );
